7dfb7781eb5ddd7641a974728838d5d616081d2b
damasukma <damasukmath@gmail.com>
2023-09-14 23:08:14 +0700

    meta

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Filonef Docs</span>,
@@ -13,6 +15,24 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Filonef Docs',
   },
+  head: ()=> {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://docs.filonef.id' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Filonef Docs'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Find user guides, code samples, SDKs & toolkits, tutorials, API & CLI references, and more.'}
+        />
+      </>
+    )
+  }
 }
 
 export default config
